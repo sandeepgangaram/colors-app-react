aa48e6a15b645844aa0f4391c67fb950fa02d557
unknown <sandeepgangaram@alumni.iitm.ac.in>
2022-04-14 17:07:56 +0530

    fixed Palette Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import Palette from "./Palette";
-import seedColors from "./seedColors";
 
-import { generatePalette } from "./colorHelpers";
 import { Routes, Route, Link } from "react-router-dom";
 function App() {
-  console.log(generatePalette(seedColors[4]));
   return (
     <>
       <Routes>
         <Route path="/" element={<h1>Palette Home</h1>} />
-        <Route path="/palette/:id" element={<h1>Individual Palette</h1>} />
+        <Route path="/palette/:id" element={<Palette />} />
       </Routes>
       {/* <div>
       <Palette palette={generatePalette(seedColors[4])} />
--- src/Palette.js
+++ src/Palette.js
@@ -5,7 +5,21 @@ import NavBar from "./NavBar";
 
 import "./Palette.css";
 
-const Palette = (props) => {
+import seedColors from "./seedColors";
+
+import { generatePalette } from "./colorHelpers";
+
+import { useParams } from "react-router-dom";
+
+function findPalette(id) {
+  return seedColors.find((palette) => {
+    return palette.id === id;
+  });
+}
+const Palette = () => {
+  const params = useParams();
+  const palette = generatePalette(findPalette(params.id));
+
   const [level, setLevel] = useState(500);
   const [format, setFormat] = useState("hex");
   const changeLevel = (level) => {
@@ -15,7 +29,7 @@ const Palette = (props) => {
   const changeColorFormat = (val) => {
     setFormat(val);
   };
-  const colorBoxes = props.palette.colors[level].map((color) => (
+  const colorBoxes = palette.colors[level].map((color) => (
     <ColorBox background={color[format]} name={color.name} key={color.id} />
   ));
 
@@ -28,8 +42,8 @@ const Palette = (props) => {
       />
       <div className="Palette-colors">{colorBoxes}</div>
       <footer className="Palette-footer">
-        {props.palette.paletteName}
-        <span className="emoji">{props.palette.emoji}</span>
+        {palette.paletteName}
+        <span className="emoji">{palette.emoji}</span>
       </footer>
     </div>
   );
