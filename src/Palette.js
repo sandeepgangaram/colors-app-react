e11457c8b0b018bdaaa9ebc9eb4b2f929b68a1f0
unknown <sandeepgangaram@alumni.iitm.ac.in>
2022-04-14 11:33:38 +0530

    CRA

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      {/* Navbar */}
+      <div className="Palette-colors">{/* Color Boxes */}</div>
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Palette;
