ed8fe48f16a5d4ab585a38eb29a9392c03fba2ff
unknown <sandeepgangaram@alumni.iitm.ac.in>
2022-04-14 16:19:30 +0530

    add Snackbar and styled select

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,13 +5,36 @@ import "./NavBar.css";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 
+import Snackbar from "@mui/material/Snackbar";
+import CloseIcon from "@mui/icons-material/Close";
+import IconButton from "@mui/material/IconButton";
+
 const NavBar = (props) => {
   const { level, changeLevel, handleChange } = props;
   const [format, setFormat] = useState("hex");
+  const [open, setOpen] = useState(false);
+
   const changeHandler = (e) => {
     setFormat(e.target.value);
     handleChange(e.target.value);
+    setOpen(true);
   };
+
+  const handleClose = (event, reason) => {
+    setOpen(false);
+  };
+
+  const action = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      key="close"
+      color="inherit"
+      onClick={handleClose}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
   return (
     <header className="Navbar">
       <div className="logo">
@@ -36,6 +59,17 @@ const NavBar = (props) => {
           <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={open}
+        autoHideDuration={2300}
+        onClose={handleClose}
+        message={
+          <span id="message-id">Format Changed to {format.toUpperCase()}</span>
+        }
+        action={action}
+        ContentProps={{ "aria-describedby": "message-id" }}
+      />
     </header>
   );
 };
