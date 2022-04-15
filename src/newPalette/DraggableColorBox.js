d57db2d2e9085025eceea4748008f137f352e527
unknown <sandeepgangaram@alumni.iitm.ac.in>
2022-04-15 11:52:41 +0530

    style DraggableColorBox

--- src/newPalette/DraggableColorBox.js
+++ src/newPalette/DraggableColorBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { styled } from "@mui/system";
-
+import DeleteIcon from "@mui/icons-material/Delete";
 const Main = styled("div")({
   inlineSize: "20%",
   blockSize: "25%",
@@ -10,9 +10,37 @@ const Main = styled("div")({
   position: "relative",
   cursor: "pointer",
   marginBlockEnd: "-4px",
+  "&:hover .delete-icon": {
+    color: "white",
+    transform: "scale(1.25)",
+  },
+  "& #box-content": {
+    position: "absolute",
+    inlineSize: "100%",
+    insetInlineStart: "0px",
+    insetBlockEnd: "0px",
+    padding: "10px",
+    color: "rgba(0,0,0,0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  "& .delete-icon": {
+    transition: "all 0.3s ease-in-out",
+  },
 });
 const DraggableColorBox = ({ color, name }) => {
-  return <Main style={{ backgroundColor: color }}>{name}</Main>;
+  return (
+    <Main style={{ backgroundColor: color }}>
+      <div id="box-content">
+        <span>{name}</span>
+
+        <DeleteIcon className="delete-icon" />
+      </div>
+    </Main>
+  );
 };
 
 export default DraggableColorBox;
