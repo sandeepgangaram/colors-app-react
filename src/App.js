c8df784110eedb3f83f9bee98ebb666d9022b709
unknown <sandeepgangaram@alumni.iitm.ac.in>
2022-04-15 18:29:28 +0530

    save Palettes to local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
@@ -7,11 +7,16 @@ import seedColors from "./seedColors";
 import Palette from "./Palette";
 
 function App() {
-  const [allPalette, setAllPalette] = useState(seedColors);
+  const savedColors = JSON.parse(window.localStorage.getItem("palett"));
+
+  const [allPalette, setAllPalette] = useState(savedColors || seedColors);
   const savePalette = (newPalette) => {
     setAllPalette((prev) => [...prev, newPalette]);
-    console.log(newPalette);
   };
+
+  useEffect(() => {
+    window.localStorage.setItem("palett", JSON.stringify(allPalette));
+  }, [allPalette]);
   return (
     <>
       <Routes>
