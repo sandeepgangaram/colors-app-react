37b28491815d34b4ee3414311eac02e6a911e9b9
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-06-19 14:26:57 +0530

    Updates

---
+++ precache-manifest.3c82f62753733c6ea1843a7f0de1a8de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06ba264c00f1a6ea975",
+    "url": "/colors-app-react/static/css/main.17a3df6b.chunk.css"
+  },
+  {
+    "revision": "b06ba264c00f1a6ea975",
+    "url": "/colors-app-react/static/js/main.b06ba264.chunk.js"
+  },
+  {
+    "revision": "5545d045c0f52ce03c57",
+    "url": "/colors-app-react/static/js/1.5545d045.chunk.js"
+  },
+  {
+    "revision": "5d031904325be045114f",
+    "url": "/colors-app-react/static/css/2.526fc807.chunk.css"
+  },
+  {
+    "revision": "5d031904325be045114f",
+    "url": "/colors-app-react/static/js/2.5d031904.chunk.js"
+  },
+  {
+    "revision": "66caf38b7cc076a10063",
+    "url": "/colors-app-react/static/js/runtime~main.66caf38b.js"
+  },
+  {
+    "revision": "6020d7234337b3e781a6067b1587c81e",
+    "url": "/colors-app-react/static/media/bg.6020d723.svg"
+  },
+  {
+    "revision": "b2d3f84e76b899532324419beb2e4423",
+    "url": "/colors-app-react/index.html"
+  }
+];
